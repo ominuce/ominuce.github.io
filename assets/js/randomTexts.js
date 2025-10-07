661d67d55c9071ac0d32fc3500e8f66434036eb4
ominuce <thierry.lcle@gmail.com>
2025-10-08 01:13:03 +0200

    :'(

--- assets/js/randomTexts.js
+++ assets/js/randomTexts.js
@@ -1,6 +1,19 @@
 (function () {
-  const text = document.getElementById("myText")
-  myText.addEventListener('click', function () {
-    text.textContent = "mdr"
-  });
-  })();
+  const texts = [
+  "«[...]je peux dire sans hésiter que c’est un profil rare.»",
+  "«[...]apporte à la fois de la rigueur, de l’innovation et de la fraîcheur à une équipe.»",
+  "«[...]structurer des systèmes solides tout en proposant des twists et des idées originales qui enrichissent et dynamisent l’expérience.»",
+  "«C’est une personne fiable sur qui on peut se reposer pour planifier et avancer sereinement dans la production.»",
+  "«Je la recommande très vivement pour un poste de Game Designer, Level Designer, ou les deux.»",
+  "«[...]passionnée par le jeu vidéo.»",
+  "n'hésitez pas à me contacter"
+];
+
+const chaine1 = texts [0]
+console.log("premiere entrée " + texts)
+myText.addEventListener('click', function() {
+  texts.textContent = texts[Math.floor(Math.random()*texts.length)]
+  console.log("premiere entrée " + texts)
+
+});
+});
