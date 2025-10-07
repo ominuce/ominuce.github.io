efb09d36749bf365d1d0e1bccc3bdc87de7a687a
ominuce <thierry.lcle@gmail.com>
2025-10-07 23:29:56 +0200

    button changement de textes

--- assets/js/randomTexts.js
+++ assets/js/randomTexts.js
@@ -1,1 +1,8 @@
-randomTexts.js
+(function () {
+  const button = document.getElementById("button id")
+  button.addEventListener('click', function () {
+    button.textContent = "nouveau texte"
+    button.
+  });
+})();
+
